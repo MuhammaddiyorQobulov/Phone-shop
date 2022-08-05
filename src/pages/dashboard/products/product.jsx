5e1c8d091652ad85863faa82ca254d4e69be73d6
MuhammaddiyorQobulov <muhammaddiyor20032808@gmail.com>
2022-08-05 18:48:27 +0500

    ffff

--- src/pages/dashboard/products/product.jsx
+++ src/pages/dashboard/products/product.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
-
+import { bagAdd } from "../../../assets/icons";
 class Product extends Component {
   render() {
+    const {
+      id,
+      name,
+      model,
+      imgURL,
+      rate,
+      price,
+      description,
+      countOfProducts,
+    } = this.props.product;
     return (
-      <div className="product">{console.log(this.props.product.name)}</div>
+      <div className="product">
+        <img src={imgURL} alt="" />
+        <h3>{name}</h3>
+        <p>{model}</p>
+        <div className="price">
+          <b>$ {price}</b>
+          <button>{bagAdd}</button>
+        </div>
+        {console.log(this.props.product.name)}
+      </div>
     );
   }
 }
