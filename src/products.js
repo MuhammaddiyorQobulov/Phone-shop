547f9c3b3cdfb90dcb70501791403dd421667213
MuhammaddiyorQobulov <muhammaddiyor20032808@gmail.com>
2022-08-05 16:16:24 +0500

    changed bag section

--- src/products.js
+++ src/products.js
@@ -1,5 +1,4 @@
 import { faker } from "@faker-js/faker";
-
 const products = [
   {
     id: faker.random.numeric(5),
@@ -9,6 +8,17 @@ const products = [
     rate: faker.datatype.number({ max: 10 }) / 2,
     price: faker.commerce.price(),
     description: faker.random.words(200),
+    countOfProduc: 0,
+  },
+  {
+    id: faker.random.numeric(5),
+    name: "Sony ZX33OBT",
+    model: "Light",
+    imgURL: require("./assets/images/Dell-XPS-13-Black.png"),
+    rate: faker.datatype.number({ max: 10 }) / 2,
+    price: faker.commerce.price(),
+    description: faker.random.words(200),
+    countOfProduc: 0,
   },
 ];
 
