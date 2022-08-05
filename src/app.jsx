e5425f778f8b415ac4a12ea9a11ac95403c1bf44
Jovliyev Inomjon <91968320+Inomjon7777@users.noreply.github.com>
2022-08-05 22:21:21 +0500

    inomjon finished

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,14 @@ import { Component } from "react";
 import Dashboard from "./pages/dashboard/dashboard";
 import Login from "./pages/login/login";
 import Products from "./pages/dashboard/products/products";
+import BagItems from "./pages/bag-items/bag-items";
 class App extends Component {
   state = {};
 
   render() {
     return (
       <div className="App">
+        <BagItems />
         <Products />
         <Login />
         <Dashboard />
--- src/pages/bag-items/bag-items.jsx
+++ src/pages/bag-items/bag-items.jsx
@@ -1,19 +1,35 @@
-import React, { Component } from 'react'
-import SideBar from '../../components/side-bar/side-bar';
+import React, { Component } from "react";
+import SideBar from "../../components/side-bar/side-bar";
+//rasmlar import qilindi
+import DellXPS13White from "../../assets/images/Dell-XPS-13-White.png";
+import { star, starHalf } from "../../assets/icons";
+import products from "./../../products";
+import BagItem from "./bag-item";
+import "./bag-item.scss";
 
-import "./bag.scss";
 
-export default class BagItems extends Component {
+class BagItems extends Component {
   render() {
     return (
       <div className="dashboard">
         <div className="left-sidebar">
           <SideBar />
         </div>
-        <div className="main-wrapper">Bu yerda products keladi</div>
+        <div className="main-wrapper">
+          <div className="bag-items">
+            <div className="bag-items-boxes">
+              <h1 className="bag-items__title">Check your Bag Items</h1>
+              {products.map((product) => (
+                <BagItem product= {product}/>
+              )) }
+            </div>
+          </div>
+        </div>
         <div className="right-sidebar">Bu yerda bag companenta keladi</div>
       </div>
       // Componentlar shu divlar ichida yoziladi !!!
-    )
+    );
   }
 }
+
+export default BagItems;
