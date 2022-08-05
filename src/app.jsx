79823f237e02c8382b4f2c9f6c0af4baf4ae8ef2
Samandar <samandarabdullaev393@gmail.com>
2022-08-05 18:59:23 +0500

    added btn section

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,14 @@ import { Component } from "react";
 import Dashboard from "./pages/dashboard/dashboard";
 import Login from "./pages/login/login";
 import Products from "./pages/dashboard/products/products";
+import Button from "./components/button/button";
 class App extends Component {
   state = {};
 
   render() {
     return (
       <div className="App">
+        <Button title="Samandar" logo="++" />
         <Products />
         <Login />
         <Dashboard />
