f260e9f33802611d036edc6265d1da96b6c39bd7
MuhammaddiyorQobulov <muhammaddiyor20032808@gmail.com>
2022-08-06 12:23:27 +0500

    added bag section

--- src/pages/bag-items/bag-items.jsx
+++ src/pages/bag-items/bag-items.jsx
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
 import Bag from "../../components/bag/bag";
-import SideBar from "../../components/side-bar/side-bar";
-=======
 import SideBar from "../../components/side-bar/side-bar";
 import products from "./../../products";
 import BagItem from "./bag-item";
 import "./bag-item.scss";
->>>>>>> e7b74427fbb761d5d656a9b41d4996d234ac8242
 
 class BagItems extends Component {
   render() {
@@ -16,13 +12,6 @@ class BagItems extends Component {
         <div className="left-sidebar">
           <SideBar />
         </div>
-<<<<<<< HEAD
-        <div className="main-wrapper">Bu yerda products keladi</div>
-        <Bag />
-        <div className="right-sidebar">Bu yerda bag companenta keladi</div>
-      </div>
-      // Componentlar shu divlar ichida yoziladi !!!
-=======
         <div className="main-wrapper">
           <div className="bag-items ">
             <div className="bag-items-boxes">
@@ -33,9 +22,10 @@ class BagItems extends Component {
             </div>
           </div>
         </div>
-        <div className="right-sidebar">Bu yerda bag companenta keladi</div>
+        <div className="right-sidebar">
+          <Bag />
+        </div>
       </div>
->>>>>>> e7b74427fbb761d5d656a9b41d4996d234ac8242
     );
   }
 }
